fcf8223ba9cbe0aebb8bd53134be710243d6a3e4
supermonkey74 <80603881+supermonkey74@users.noreply.github.com>
2021-08-07 08:44:11 -0500

    Modal changes

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("Modal-training");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
